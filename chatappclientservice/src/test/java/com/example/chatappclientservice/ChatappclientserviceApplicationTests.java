562121878727a1f87054ba46747b20be0527738d
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-10-01 15:19:29 +0530

    intialize

---
+++ chatappclientservice/src/test/java/com/example/chatappclientservice/ChatappclientserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappclientserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
