6d60acc49bc2d436ead149ec83734a1646e45242
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-10-07 15:30:45 +0530

    server side code is run complete
    firstly, run the server than client is run where kill the port
    when client is run second time  but server is start pehle se hi than all code is run perfectly but first time server is run and client is run where registrtation code is not run

---
+++ chatappclientservice/src/main/java/com/example/chatappclientservice/ChatappclientserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappclientserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappclientserviceApplication.class, args);
+	}
+
+}
